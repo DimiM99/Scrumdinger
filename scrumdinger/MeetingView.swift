99570a7a8d1e3777722216d64d9853fd500f339d
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-06-20 01:01:26 +0200

    Initial Commit

---
+++ scrumdinger/MeetingView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  scrumdinger
+//
+//  Created by Dmitri Moscoglo on 20.06.24.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(
+                value: 5,
+                total: 15
+            )
+            HStack {
+                VStack(
+                    alignment: .leading,
+                    content: {
+                        Text("Seconds Elapsed")
+                            .font(.caption)
+                        Label("300", systemImage: "hourglass.tophalf.fill")
+                    }
+                )
+                Spacer()
+                VStack(
+                    alignment: .trailing,
+                    content: {
+                        Text("Seconds Remaining")
+                            .font(.caption)
+                        Label("600", systemImage: "hourglass.bottomhalf.fill")
+                    }
+                )
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time Remianing")
+            .accessibilityValue("10 Minutes")
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next Speaker")
+            }
+        }.padding(10)
+    }
+}
+
+#Preview {
+    MeetingView()
+}
---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Dmitri Moscoglo on 20.06.24.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
