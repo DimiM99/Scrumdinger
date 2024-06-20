e5a14c73cedb62d4abb28ed1b24ca2a643aee3ba
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-06-20 19:33:36 +0200

    created scrums view

---
+++ scrumdinger/ScrumsView.swift
@@ -0,0 +1,22 @@
+//
+//  ScrumsView.swift
+//  scrumdinger
+//
+//  Created by Dmitri Moscoglo on 20.06.24.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List (scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
