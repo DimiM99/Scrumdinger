d21186dc211270692d7bbadb1cb55ac3dd36e24f
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-06-21 00:10:25 +0200

    added a theme picker view

---
+++ scrumdinger/Views/Screens/ThemePicker.swift
@@ -0,0 +1,27 @@
+//
+//  ThemePicker.swift
+//  scrumdinger
+//
+//  Created by Dmitri Moscoglo on 21.06.24.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    
+    @Binding var selection: Theme
+    
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+        .pickerStyle(.navigationLink)
+    }
+}
+
+#Preview {
+    ThemePicker(selection: .constant(.periwinkle))
+}
