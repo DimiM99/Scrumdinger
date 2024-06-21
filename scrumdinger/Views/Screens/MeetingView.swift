d78e10325d11b71b55f86f5cee9ef0ea9d650f34
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-06-21 09:29:05 +0200

    created an overlay view for meeting view, adapted detail view for that

--- scrumdinger/Views/Screens/MeetingView.swift
+++ scrumdinger/Views/Screens/MeetingView.swift
@@ -8,48 +8,58 @@
 import SwiftUI
 
 struct MeetingView: View {
+    
+    @Binding var scum: DailyScrum
+    
     var body: some View {
-        VStack {
-            ProgressView(
-                value: 5,
-                total: 15
-            )
-            HStack {
-                VStack(
-                    alignment: .leading,
-                    content: {
-                        Text("Seconds Elapsed")
-                            .font(.caption)
-                        Label("300", systemImage: "hourglass.tophalf.fill")
-                    }
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scum.theme.mainColor)
+            VStack {
+                ProgressView(
+                    value: 5,
+                    total: 15
                 )
-                Spacer()
-                VStack(
-                    alignment: .trailing,
-                    content: {
-                        Text("Seconds Remaining")
-                            .font(.caption)
-                        Label("600", systemImage: "hourglass.bottomhalf.fill")
-                    }
-                )
-            }
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel("Time Remianing")
-            .accessibilityValue("10 Minutes")
-            Circle()
-                .strokeBorder(lineWidth: 24)
-            HStack {
-                Text("Speaker 1 of 3")
-                Spacer()
-                Button(action: {}) {
-                    Image(systemName: "forward.fill")
+                HStack {
+                    VStack(
+                        alignment: .leading,
+                        content: {
+                            Text("Seconds Elapsed")
+                                .font(.caption)
+                            Label("300", systemImage: "hourglass.tophalf.fill")
+                        }
+                    )
+                    Spacer()
+                    VStack(
+                        alignment: .trailing,
+                        content: {
+                            Text("Seconds Remaining")
+                                .font(.caption)
+                            Label("600", systemImage: "hourglass.bottomhalf.fill")
+                        }
+                    )
                 }
-                .accessibilityLabel("Next Speaker")
+                .accessibilityElement(children: .ignore)
+                .accessibilityLabel("Time Remianing")
+                .accessibilityValue("10 Minutes")
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                HStack {
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action: {}) {
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel("Next Speaker")
+                }
             }
-        }.padding(10)
+        }
+        .padding()
+        .foregroundColor(scum.theme.accentColor)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    MeetingView()
+    MeetingView(scum: .constant(DailyScrum.sampleData[0]))
 }
