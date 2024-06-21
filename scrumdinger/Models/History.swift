fb37432c8fe328f90dc5b391c635345fa2582e87
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-06-21 23:46:21 +0200

    making automatic date assignemnt on history creation

--- scrumdinger/Models/History.swift
+++ scrumdinger/Models/History.swift
@@ -12,7 +12,7 @@ struct History: Identifiable {
     let date: Date
     let attendees: [DailyScrum.Attendee]
     
-    init(id: UUID = UUID(), date: Date, attendees: [DailyScrum.Attendee]) {
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
         self.id = id
         self.date = date
         self.attendees = attendees
